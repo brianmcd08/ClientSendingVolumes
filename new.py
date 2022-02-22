85f36caaf86f495a27eb31379ec747c681ae6bee
brianmcd08 <brianmcd08@gmail.com>
2022-02-22 16:37:23 -0500

    Removed some unnecessary lines.

--- new.py
+++ new.py
@@ -14,59 +14,7 @@ def read_site_company(filename):
             siteCompany[row[1]] = row[3]
 
     return siteCompany
-        
 
-# def read_file(filename, siteCompany):
-#     # column mappings:
-#     # Company name -> newRow[1]
-#     # Volume -> newRow[2]
-#     # Year -> newRow[3]
-#     # Month -> newRow[4]
-#
-#     with open(filename, newline='') as csvfile:
-#         fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
-#         currentRow = ['', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#         newRows = []
-#
-#         # skip first row
-#         firstRow = next(fileReader)
-#
-#         for newRow in fileReader:
-#             year = int(newRow[3])
-#             month = int(newRow[4])
-#             volume = int(newRow[2])
-#             try:
-#                 company = siteCompany[newRow[1]]
-#             except KeyError:
-#                 print(f"Site {newRow[1]} not found in company mapping")
-#
-#             # check if we're at a new company
-#             # if we are, then append currentRow to newRows []
-#             # and reset currentRow
-#             if company != currentRow[0]:
-#                 # append currentRow to newRows[] only if currentRow[0] != ''
-#                 if currentRow[0] != '':
-#                     newRows.append(currentRow)
-#
-#                 # reset currentRow
-#                 currentRow = [company, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#
-#             # if 2022 (Jan), then it's our 13th column
-#             if year == 2022:
-#                 if month == 1:
-#                     month = 13
-#                 else:
-#                     month = 14
-#
-#             currentRow[month] += volume
-#
-#             # add total volume
-#             currentRow[15] += volume
-#
-#         newRows.append(currentRow)
-#         csvfile.close()
-#
-#         return newRows
 
 def process_data(rows):
     # column mappings:
